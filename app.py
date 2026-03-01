bebc426c6d5f27cc529036915595fe6dca2a8727
Tan Yency <yencytan0505@gmail.com>
2026-03-01 23:09:20 +0800

    Add ingest route for cloud collection

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask,jsonify
 import sqlite3
 import pandas as pd
-from flask import request
 
 import requests
 import time
@@ -331,55 +330,10 @@ def real_departure_summary():
         for row in data
     ])
 
-
-@app.route("/api/ingest", methods=["POST"])
-def ingest():
-
-    data = request.json
-
-    conn = sqlite3.connect(DB)
-    cursor = conn.cursor()
-
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS flights (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp TEXT,
-            icao24 TEXT,
-            flight_number TEXT,
-            origin_country TEXT,
-            latitude REAL,
-            longitude REAL,
-            baro_altitude REAL,
-            velocity REAL
-        )
-    """)
-
-    cursor.execute("""
-        INSERT INTO flights (
-            timestamp, icao24, flight_number,
-            origin_country, latitude, longitude,
-            baro_altitude, velocity
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    """, (
-        data["timestamp"],
-        data["icao24"],
-        data["flight_number"],
-        data["origin_country"],
-        data["latitude"],
-        data["longitude"],
-        data["baro_altitude"],
-        data["velocity"]
-    ))
-
-    conn.commit()
-    conn.close()
-
-    return {"status": "success"}, 200
-
 scheduler = BackgroundScheduler()
 scheduler.add_job(fetch_real_departures_job, 'interval', minutes=5)
 scheduler.start()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port) 
